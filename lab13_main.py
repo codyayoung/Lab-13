0b7509c740981b54fbab54f16f860c47da7938ad
Cody Young <cyoung1623@gmail.com>
2018-12-02 16:16:14 -0800

    Added comments for function prototope.

--- lab13_main.py
+++ lab13_main.py
@@ -3,6 +3,19 @@
 # CST 205
 # 2018-12-4
 
-# Problem 1
-# Problem 2 should be turned in as a separate Python file, copied from Lab 12.
+# Problem 1 - Mad Libs
+# Takes a source text file, removes some words from its strings, and then prompts
+# user for word to create a funny mad lib.
+
+def mad_lib():
+    # Parse input text file, store as list of separate words (input string)
+    # Get user prompts for strings (noun, adverb, adjective, etc.), store as list of strings (user strings)
+    # For each word in list of user strings, replace word in input string
+    # Return resulting string
+    return
+
+
+
+
+
 
